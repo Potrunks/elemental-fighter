d52ec61332002a80762486d4232b20ef2810a642
Potrunks <82607603+Potrunks@users.noreply.github.com>
2022-04-29 16:41:01 +0200

    Create option button on main menu

--- Assets/Script/Menu/MainMenu.cs
+++ Assets/Script/Menu/MainMenu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -13,5 +11,10 @@ public class MainMenu : MonoBehaviour
     public void QuitButtonScript()
     {
         Application.Quit();
+    }
+
+    public void OptionButtonScript()
+    {
+        // TO DO
     }
 }
