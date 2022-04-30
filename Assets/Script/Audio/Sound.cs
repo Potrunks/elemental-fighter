c657749736c346d040ae5105e5197a4b2e1e1378
Potrunks <82607603+Potrunks@users.noreply.github.com>
2022-04-30 12:29:44 +0200

    Upgrade amelioration of the volume settings of option menu from main menu
    When comeback to the main menu from a battleground, the value of the main them volume slider is preserved

--- Assets/Script/Audio/Sound.cs
+++ Assets/Script/Audio/Sound.cs
@@ -6,8 +6,6 @@ public class Sound
 {
     public string name;
     public AudioClip audioClip;
-    [Range(0f, 1f)]
-    public float volume;
     [Range(0.1f, 3f)]
     public float pitch;
     public bool loop;
--- Assets/Script/Menu/OptionMenu.cs
+++ Assets/Script/Menu/OptionMenu.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionMenu : MonoBehaviour
 {
     public GameObject mainMenu;
+    public Slider mainSlider;
     public AudioManager audioManager;
+    private void Awake()
+    {
+        mainSlider.value = GameManager.instance.volumeMainTheme;
+    }
     public void BackButtonScript()
     {
         this.gameObject.SetActive(false);
         mainMenu.SetActive(true);
     }
-    public void SetMainThemeVolume(float sliderVolumeMainTheme) {
+    public void SetMainThemeVolume(float sliderVolumeMainTheme)
+    {
         GameManager.instance.volumeMainTheme = sliderVolumeMainTheme;
         Sound sound = audioManager.FindSoundByName("MainTheme");
-        if(sound != null) {
+        if (sound != null)
+        {
             sound.audioSource.volume = GameManager.instance.volumeMainTheme;
         }
     }
