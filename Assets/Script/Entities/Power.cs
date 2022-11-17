2e9ab89c66aa62e92a71da061455b9ca74ac2b0d
Potrunks <82607603+Potrunks@users.noreply.github.com>
2022-11-18 00:07:28 +0100

    Create scriptable object power

---
+++ Assets/Script/Entities/Power.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Script.Entities
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "New Power", menuName = "Power/New Object")]
+    public class Power : ScriptableObject
+    {
+        public string powerName;
+        public string description;
+        public string type;
+        public float speed;
+        public float damage;
+        public GameObject model;
+    }
+}
