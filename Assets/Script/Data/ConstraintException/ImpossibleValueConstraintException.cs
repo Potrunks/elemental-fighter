40bd8e66a9dbd1a00d2255471bb14432d16a4b99
Potrunks <potrunks@hotmail.com>
2022-12-19 23:48:06 +0100

    Refactor and clean #93

--- Assets/Script/Data/ConstraintException/ImpossibleValueConstraintException.cs
+++ Assets/Script/Data/ConstraintException/ImpossibleValueConstraintException.cs
@@ -4,8 +4,6 @@ namespace Assets.Script.Data.ConstraintException
 {
     public class ImpossibleValueConstraintException : Exception
     {
-        public ImpossibleValueConstraintException() { }
-
         public ImpossibleValueConstraintException(string message, float indexPlayer) : base(String.Format(message, indexPlayer)) { }
     }
 }
