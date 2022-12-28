13518d0fa252811b2909e26b9aa756208f3b7840
Potrunks <potrunks@hotmail.com>
2022-12-29 00:05:49 +0100

    Create fireball controller and state #103

---
+++ Assets/Script/Controller/Power/Fire/FireballController.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Script.FiniteStateMachine.Power.Implementation.Fire;
+
+namespace Assets.Script.Controller.Power.Fire
+{
+    public class FireballController : PowerController
+    {
+        #region Monobehaviour Method
+        private void Start()
+        {
+            currentState = new FireballCastState();
+            currentState.OnEnter(this);
+        }
+        #endregion
+    }
+}
---
+++ Assets/Script/FiniteStateMachine/Power/Implementation/Fire/FireballDestroyState.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Script.Data.Reference;
+using System;
+using UnityEngine;
+
+namespace Assets.Script.FiniteStateMachine.Power.Implementation.Fire
+{
+    public class FireballDestroyState : PowerState
+    {
+        public override IPowerState CheckingStateModification(PowerController powerController)
+        {
+            if (powerController._animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                GameObject.Destroy(powerController.gameObject);
+                return null;
+            }
+
+            return nextState;
+        }
+
+        public override void OnEnter(PowerController powerController)
+        {
+            powerController._animator.Play("Destroying");
+            if (powerController._isDestroyedAfterDestructiveCollision)
+            {
+                powerController._audioBusiness.PlayRandomSoundEffect(SoundEffectType.ELEMENTAL_DESTROYING, powerController._soundEffectByType);
+            }
+        }
+
+        public override void OnExit(PowerController powerController)
+        {
+            
+        }
+    }
+}
