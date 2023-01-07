3dfd130c48982b59782a40950522eed57483df3e
Potrunks <potrunks@hotmail.com>
2023-01-07 02:26:13 +0100

    create die state fire warrior #108

---
+++ Assets/Script/FiniteStateMachine/PlayableCharacter/Implementation/Fire/FireWarriorDieState.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Script.Data;
+using UnityEngine;
+
+namespace Assets.Script.FiniteStateMachine.PlayableCharacter.Implementation.Fire
+{
+    public class FireWarriorDieState : PlayableCharacterStateV2
+    {
+        private IPlayableCharacterStateV2 nextState;
+
+        public override IPlayableCharacterStateV2 CheckingStateModification(PlayableCharacterController playableCharacterController)
+        {
+            if (playableCharacterController.playableCharacterAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                bool gameIsOver = playableCharacterController.characterBusiness.ResetCharacterAfterDeath(playableCharacterController);
+                if (!gameIsOver)
+                {
+                    return new FireWarriorIdleState();
+                }
+            }
+
+            return nextState;
+        }
+
+        public override void OnEnter(PlayableCharacterController playableCharacterController)
+        {
+            playableCharacterController.playableCharacterMoveSpeed = 0;
+            playableCharacterController.playableCharacterAnimator.Play("Die");
+        }
+
+        public override void OnExit(PlayableCharacterController playableCharacterController)
+        {
+            
+        }
+
+        public override void PerformingInput(PlayableCharacterActionReference action)
+        {
+            switch (action)
+            {
+                default:
+                    Debug.LogWarning(GamePlayConstraintException.ActionNotPermitted + action);
+                    nextState = null;
+                    break;
+            }
+        }
+    }
+}
