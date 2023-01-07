19274a22481df4e5fa8cbd546a1c375dcc3da2e6
Potrunks <potrunks@hotmail.com>
2023-01-07 13:30:15 +0100

    Create dash state #113

---
+++ Assets/Script/FiniteStateMachine/PlayableCharacter/Implementation/Fire/FireWarriorDashState.cs
@@ -0,0 +1,62 @@
+﻿using Assets.Script.Controller.PlayableCharacter.Fire;
+using Assets.Script.Data;
+using UnityEngine;
+
+namespace Assets.Script.FiniteStateMachine.PlayableCharacter.Implementation.Fire
+{
+    public class FireWarriorDashState : PlayableCharacterStateV2
+    {
+        private IPlayableCharacterStateV2 nextState;
+
+        public override IPlayableCharacterStateV2 CheckingStateModification(PlayableCharacterController playableCharacterController)
+        {
+            if (playableCharacterController._isTouchingByAttack)
+            {
+                return new FireWarriorHurtState();
+            }
+
+            if (playableCharacterController.isGrounding)
+            {
+                return new FireWarriorIdleState();
+            }
+
+            if (playableCharacterController.playableCharacterAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                if (playableCharacterController.isGrounding)
+                {
+                    return new FireWarriorIdleState();
+                }
+
+                return new FireWarriorFallState();
+            }
+
+            return nextState;
+        }
+
+        public override void OnEnter(PlayableCharacterController playableCharacterController)
+        {
+            FirePlayableCharacterController character = (FirePlayableCharacterController)playableCharacterController;
+            character.playableCharacterMoveSpeed = 0;
+            character.playableCharacterAnimator.Play("DashMove");
+            playableCharacterController.playableCharacterRigidbody.AddForce(new Vector2(playableCharacterController.playableCharacter.JumpForce * GamePlayValueReference.DASH_FORCE_MULTIPLICATOR * (playableCharacterController._isLeftFlip ? -1 : 1), 0));
+            character._dashVFX.Play();
+        }
+
+        public override void OnExit(PlayableCharacterController playableCharacterController)
+        {
+            playableCharacterController.playableCharacterMoveSpeed = playableCharacterController.playableCharacter.MoveSpeed;
+            playableCharacterController._isTouchingByAttack = false;
+        }
+
+        public override void PerformingInput(PlayableCharacterActionReference action)
+        {
+            switch (action)
+            {
+                default:
+                    Debug.LogWarning(GamePlayConstraintException.ActionNotPermitted + action);
+                    nextState = null;
+                    break;
+            }
+        }
+    }
+}
